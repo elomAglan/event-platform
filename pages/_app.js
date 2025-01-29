ce35f8545f3c072b31fef67e69d2306dc75bb82e
elomAglan <156069899+elomAglan@users.noreply.github.com>
2025-01-29 05:46:45 +0000

    Delete pages directory

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "bulma/css/bulma.min.css"; // Importation de Bulma CSS
-import "../styles/globals.css"; // Importation de vos autres styles globaux si nécessaire
-import "animate.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
